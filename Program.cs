f22b4091fe941af4eab851dbbfce8af6a93cc3da
Connorjt <ct770a@gmail.com>
2023-10-05 20:10:18 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace InfiniteTextureCompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+
+
+
+        static void convert_image(string file)
+        {
+            System.Drawing.Image image1 = System.Drawing.Image.FromFile(file);
+            var v = image1.PixelFormat;
+
+
+        }
+    }
+}
